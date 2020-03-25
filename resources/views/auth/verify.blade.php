37e47d8f5e57812ae7bbb685f94b48f0b67fb475
melon <ememendeztabilo@gmail.com>
2020-03-25 03:16:17 -0300

    layout name  has been refactorized

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts_backend_app')
 
 @section('content')
 <div class="container">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <link rel="stylesheet" href="/css/app.css">
+        <script src="/js/app.js"></script>
+        <title>Cesfam Dr. Thomas Fenton</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -65,6 +66,7 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+            
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -86,6 +88,7 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
+                    
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
