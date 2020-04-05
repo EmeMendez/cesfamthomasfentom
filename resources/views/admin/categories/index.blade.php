cb61e145b285cf94647bc7b24e237e1db53c8b4f
melon <ememendeztabilo@gmail.com>
2020-04-05 01:11:49 -0400

    views responsives

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,97 +1,86 @@
 @extends('layouts.admin_app')
+
 @section('content')
-<div class="container">
-  <div class="col-10 mx-auto">
+<div class="container mb-5">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header"><h4 class="mb-0 text-secondary">Lista de categorías</h4></div>
 
-  <div class="card px-5">
-  {{-- star search --}}
-    <div class="row py-3 align-items-center">
-      <div class="col-9 pl-2">
-        <p class="h4 text-secondary font-weight-bold">Lista de Categorías </p>
-      </div>
-      <div class="col-3 text-right pr-2">
-        <a href="{{route('admin.categories.create')}}" class="text-success"><button class="btn btn-small btn-success">Crear nueva</button></ins></a>        
-      </div>
-    </div>
+                <div class="card-body">
+                  <a href="{{route('admin.categories.create')}}"><button class="btn btn-success mb-3">Crear nueva +</button></a>
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-  {{-- star search --}}
-  <form action="{{route('admin.categories.index')}}" method="GET">
-    <div class="row">
-      <div class="col-11 pl-0">
-        <div class="input-group">
-          <input name="name"value="{{old('name',$name)}}" type="text" class="form-control" id="input_search" placeholder="Buscar categoría">
-        <a style="text-decoration:none" href="{{route('admin.categories.index')}}">
-          <div class="input-group-prepend">
-            <div class="input-group-text">x</div>
-          </div>
-        </a>
-        </div>        
-      </div>
-      <div class="col-1 pr-0">
-        <button class="btn btn-primary">
-          <svg class="bi bi-search text-white" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
-            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
-          </svg>          
-        </button>
-      </div>
-    </div>
-    </form>
-  {{-- end search --}}
+                    {{-- start search --}}
+                    <form action="{{route('admin.categories.index')}}" method="GET">
+                        <div class="row">
+                          <div class="col-10 col-md-11">
+                            <div class="input-group">
+                              <input name="name"value="{{old('name',$name)}}" type="text" class="form-control" id="input_search" placeholder="Buscar categoría">
+                              <a style="text-decoration:none" href="{{route('admin.categories.index')}}">
+                              <div class="input-group-prepend">
+                                <div class="input-group-text">x</div>
+                              </div>
+                            </a>
+                            </div>        
+                          </div>
+                          <div class="col-2 col-md-1 pl-0 pl-md-3">
+                            <button class="btn btn-primary">
+                              <svg class="bi bi-search text-white" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
+                                <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
+                              </svg>          
+                            </button>
+                          </div>
+                        </div>
 
+                      </form>
+                    {{-- end search --}}
 
-  @if(count($categories)>0)
-    <div class="row">
-        <div class="pt-2 pl-2">
-          <p class="text-secondary">Resuldados  @if($name) para <b>{{$name}}</b>  @endif() : [ {{$categories->total()}} ] </p>     
+                    @if(count($categories)>0)
+                    <div>
+                    <p class="text-secondary my-2">Resuldados  @if($name) para <b>{{$name}}</b>  @endif() : [ {{$categories->total()}} ] </p>     
+                    {{-- start table --}}
+                    <div class="table-responsive-sm">
+                      <table class="table mb-0">
+                        <table class="table table-striped">
+                          <thead class="text-secondary" >
+                            <tr>
+                              <th  scope="col">#</th>
+                              <th scope="col">Nombre</th>
+                              <th scope="col">&nbsp</th>
+                              <th scope="col">&nbsp</th>
+                              <th scope="col">&nbsp</th>
+                            </tr>
+                          </thead>
+                          <tbody class="text-secondary">
+                            @foreach($categories as $category)
+                            <tr>
+                              <td scope="col">{{$category->id}}</td>
+                              <td scope="col">{{$category->name}}</td>
+                              <td scope="col"><button class="btn btn-sm btn-outline-dark">Ver</button></td>
+                              <td scope="col"><button class="btn btn-sm btn-outline-info">Editar</button></td>
+                              <td scope="col"><button class="btn btn-sm btn-outline-danger">Eliminar</button>
+                              </td>
+
+                            </tr>
+                          @endforeach
+                          </tbody>
+                        </table>
+                        {{$categories->links()}}  
+                      </table>
+                    </div>                        
+                  {{-- end table --}}                
+                  </div>
+                    @else
+                    <p class="text-secondary my-2">No se encontraron resultados para la busqueda : <b>{{$name}}</b></p>     
+                    @endif
+            </div>
         </div>
-
-          <table class="table table-striped text-secondary">
-              <thead>
-                <tr>
-                  <th >ID</th>
-                  <th>Nombre</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach($categories as $category)
-                <tr>
-                  <td>{{$category->id}}</td>
-                  <td>{{$category->name}}</td>
-                  <td>                  
-                    <div class="d-inline text-right">
-                        <a href="{{route('admin.categories.show',$category->id)}}"><button class="btn btn-sm btn-outline-dark">Ver</button></a>
-                        <a href="{{route('admin.categories.edit',$category)}}"><button class="btn btn-sm btn-outline-primary">Editar</button></a>           
-                        <form class="d-inline" action="{{ route('admin.categories.destroy',$category->id) }}" method="POST">
-                          @csrf
-                          @method('DELETE')
-                          <button type="submit" class="btn btn-sm btn-outline-danger">Eliminar</button>
-                        </form>
-                    </div>
-                </td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
-            {{$categories->links()}}
-      </div>
-    @else
-      <div class="pt-2">
-        <p class="text-secondary">No se encontraron resultados para la busqueda : <b>{{$name}}</b>
-        </p>     
-      </div>
-    @endif
-
-
-
-  
-    
-  
     </div>
-</div>    
-
-
-
 </div>
 @endsection
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,126 +1,117 @@
 @extends('layouts.admin_app')
+
 @section('content')
-<div class="container">
-  <div class="col-10 mx-auto">
+<div class="container mb-5">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header"><h4 class="mb-0 text-secondary">Lista de posts</h4></div>
 
-  <div class="card px-5">
-    <div class="row py-3 align-items-center">
-      <div class="col-9 pl-2">
-        <p class="h4 text-secondary font-weight-bold">Lista de Posts </p>
-      </div>
-      <div class="col-3 text-right pr-2">
-        <a href="{{route('admin.posts.create')}}" class="text-success"><button class="btn btn-small btn-success">Crear nuevo</button></ins></a>        
-      </div>
+                <div class="card-body">
+                  <a href="{{route('admin.posts.create')}}"><button class="btn btn-success mb-3">Crear nuevo +</button></a>
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{-- start search --}}
+                    <form action="{{route('admin.posts.index')}}" method="GET">
+                      <div class="row">
+                        <div class="col-12 col-md-3 mb-2 mb-md-0 ">
+                          <div class="input-group">
+                            <input name="name"value="{{old('name',$name)}}" type="text" class="form-control" id="search" placeholder="Buscar por nombre">
+                  
+                          </div>        
+                        </div>
+                  
+                        <div class="col-12 col-md-4 mb-2 mb-md-0">
+                          <select name="category" class="form-control" id="category">
+                            <option selected disabled value="">--Seleccione categoría--</option>
+                            @foreach($categories as $cat)
+                          <option  @if(old('category',$category) == $cat->id) selected @endif value="{{$cat->id}}">{{$cat->name}}</option>
+                            @endforeach
+                          </select>         
+                        </div>
+                  
+                        <div class="col-12 col-md-3 mb-2 mb-md-0">
+                          <select name="status" class="form-control" id="status">
+                            <option selected disabled value="">--Seleccione Estado--</option>
+                            <option @if(old('status',$status) == 'PUBLISHED') selected @endif value="PUBLISHED">Publicados</option>
+                            <option @if(old('status',$status) == 'DRAFT') selected @endif value="DRAFT">Borradores</option>         
+                          </select>         
+                        </div>
+                        
+                        <div class="col-6 col-md-1 text-right text-md-left ">
+                          <button type="submit" class="btn btn-primary">
+                            <svg class="bi bi-search text-white" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
+                              <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
+                            </svg>          
+                          </button>
+                        </div>
+                  
+                        <div class="col-6 col-md-1">
+                          <a class="text-white" href="{{route('admin.posts.index')}}">
+                            <button type="button" class="btn btn-secondary">
+                              <svg class="bi bi-arrow-counterclockwise" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M12.83 6.706a5 5 0 00-7.103-3.16.5.5 0 11-.454-.892A6 6 0 112.545 5.5a.5.5 0 11.91.417 5 5 0 109.375.789z" clip-rule="evenodd"/>
+                                <path fill-rule="evenodd" d="M7.854.146a.5.5 0 00-.708 0l-2.5 2.5a.5.5 0 000 .708l2.5 2.5a.5.5 0 10.708-.708L5.707 3 7.854.854a.5.5 0 000-.708z" clip-rule="evenodd"/>
+                              </svg> 
+                            </button>
+                          </a>
+                  
+                        </div>
+                      </div>
+                      </form>
+                    {{-- end search --}}
+
+                    @if(count($posts)>0)
+                    <div>
+                    <p class="text-secondary my-2">Resuldados  @if($name) para <b>{{$name}}</b>  @endif() : [ {{$posts->total()}} ] </p>     
+                    {{-- start table --}}
+                    <div class="table-responsive-sm">
+                      <table class="table mb-0">
+                        <table class="table table-striped">
+                          <thead class="text-secondary" >
+                            <tr>
+                              <th  scope="col">#</th>
+                              <th scope="col">Nombre</th>
+                              <th scope="col">Estado</th>                   
+                              <th scope="col">&nbsp</th>
+                              <th scope="col">&nbsp</th>
+                              <th scope="col">&nbsp</th>
+                            </tr>
+                          </thead>
+                          <tbody class="text-secondary">
+                            @foreach($posts as $post)
+                            <tr>
+                              <td scope="col">{{$post->id}}</td>
+                              <td scope="col">{{$post->name}}</td>
+                              <td scope="col">@if($post->status == 'DRAFT') <span class="text-dark">Borrador<span> @else <span class="text-success">Publicado<span> @endif</td>
+                              <td scope="col"><button class="btn btn-sm btn-outline-dark">Ver</button></td>
+                              <td scope="col"><button class="btn btn-sm btn-outline-info">Editar</button></td>
+                              <td scope="col"><button class="btn btn-sm btn-outline-danger">Eliminar</button>
+                              </td>
+
+                            </tr>
+                          @endforeach
+                          </tbody>
+                        </table>
+                        {{$posts->links()}}
+                      </table>
+                    </div>                        
+                  {{-- end table --}}                
+
+                                                 
+
+                      </div>
+                    @else
+                    <p class="text-secondary my-2">No se encontraron resultados para la busqueda : <b>{{$name}}</b></p>     
+                    @endif
+            </div>
+        </div>
     </div>
-
-
-
-
-
-    {{-- star search --}}
-
-  <form action="{{route('admin.posts.index')}}" method="GET">
-    <div class="row">
-      <div class="col-3 pl-0">
-        <div class="input-group">
-          <input name="name"value="{{old('name',$name)}}" type="text" class="form-control" id="search" placeholder="Buscar por nombre">
-
-        </div>        
-      </div>
-
-      <div class="col-4 pl-0">
-        <select name="category" class="form-control" id="category">
-          <option selected disabled value="">--Seleccione categoría--</option>
-          @foreach($categories as $cat)
-        <option  @if(old('category',$category) == $cat->id) selected @endif value="{{$cat->id}}">{{$cat->name}}</option>
-          @endforeach
-        </select>         
-      </div>
-
-      <div class="col-3 pl-0">
-        <select name="status" class="form-control" id="status">
-          <option selected disabled value="">--Seleccione Estado--</option>
-          <option @if(old('status',$status) == 'PUBLISHED') selected @endif value="PUBLISHED">Publicados</option>
-          <option @if(old('status',$status) == 'DRAFT') selected @endif value="DRAFT">Borradores</option>         
-        </select>         
-      </div>
-      
-      <div class="col-1 pr-0">
-        <button type="submit" class="btn btn-primary">
-          <svg class="bi bi-search text-white" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
-            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
-          </svg>          
-        </button>
-      </div>
-
-      <div class="col-1 pr-0">
-        <a class="text-white" href="{{route('admin.posts.index')}}">
-          <button type="button" class="btn btn-secondary">
-            <svg class="bi bi-arrow-counterclockwise" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M12.83 6.706a5 5 0 00-7.103-3.16.5.5 0 11-.454-.892A6 6 0 112.545 5.5a.5.5 0 11.91.417 5 5 0 109.375.789z" clip-rule="evenodd"/>
-              <path fill-rule="evenodd" d="M7.854.146a.5.5 0 00-.708 0l-2.5 2.5a.5.5 0 000 .708l2.5 2.5a.5.5 0 10.708-.708L5.707 3 7.854.854a.5.5 0 000-.708z" clip-rule="evenodd"/>
-            </svg> 
-          </button>
-        </a>
-
-      </div>
-    </div>
-    </form>
-  {{-- end search --}}
-
-
-  @if(count($posts)>0)
-    <div class="row">
-        <div class="pt-2 pl-2">
-          <p class="text-secondary">Resuldados  @if($name) para <b>{{$name}}</b>  @endif() : [ {{$posts->total()}} ] </p>     
-        </div>     
-          <table class="table table-striped text-secondary">
-              <thead>
-                <tr>
-                  <th >ID</th>
-                  <th>Nombre</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach($posts as $post)
-                <tr>
-                  <td>{{$post->id}}</td>
-                  <td>{{$post->name}}</td>
-                  <td>                  
-                    <div class="d-inline text-right">
-                        <a href="{{route('admin.posts.show',$post->id)}}"><button class="btn btn-sm btn-outline-dark">Ver</button></a>
-                        <a href="{{route('admin.posts.edit',$post)}}"><button class="btn btn-sm btn-outline-primary">Editar</button></a>           
-                        <form class="d-inline" action="{{ route('admin.posts.destroy',$post->id) }}" method="POST">
-                          @csrf
-                          @method('DELETE')
-                          <button type="submit" class="btn btn-sm btn-outline-danger">Eliminar</button>
-                        </form>
-                    </div>
-                </td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
-            {{$posts->links()}}
-      </div>
-    @else
-      <div class="pt-2">
-        <p class="text-secondary">No se encontraron resultados para la busqueda : <b>{{$name}}</b>
-        </p>     
-      </div>
-    @endif
-
-
-
-  
-    
-  
-    </div>
-</div>    
-
-
-
 </div>
 @endsection
+
